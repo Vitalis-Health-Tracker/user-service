e107129005e9fde86eb5ff0817d918b19795ba07
silvercen <abelkm123@gmail.com>
2024-11-20 09:25:46 +0530

    Fixed update

--- src/main/java/com/example/user_service/service/UserServices.java
+++ src/main/java/com/example/user_service/service/UserServices.java
@@ -41,8 +41,8 @@ public class UserServices {
             oldUser.setHeight(newUser.getHeight());
             oldUser.setGender(newUser.getGender());
             oldUser.setJourney(newUser.getJourney());
-            Float bmi = newUser.getWeight()/(newUser.getHeight() * newUser.getHeight());
-            newUser.setBmi(bmi);
+            Float bmi = newUser.getWeight()/((newUser.getHeight()/100) * (newUser.getHeight()/100));
+            oldUser.setBmi(bmi);
             userRepo.save(oldUser);
             return oldUser;
         }
